7fe3aac8fa3a282cee7677d53babf9ddbc0193f3
Maxson Ferovante <maxsonferovante@gmail.com>
2023-06-03 18:14:40 -0300

    heroku api rest - update settings.py e views.py

--- backend/settings.py
+++ backend/settings.py
@@ -64,7 +64,7 @@ ROOT_URLCONF = 'backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -125,7 +125,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 
 # Default primary key field type
--- backend/todo/views.py
+++ backend/todo/views.py
@@ -1,8 +1,3 @@
-import os
-
-from django.http import HttpResponse, HttpResponseNotFound
-from django.shortcuts import render
-from django.views import View
 from rest_framework import viewsets
 from .serializers import TodoSerializer
 from .models import Todo
@@ -12,13 +7,3 @@ class TodoView(viewsets.ModelViewSet):
     queryset = Todo.objects.all()
 # Create your views here.
 # Add this CBV
-class Assets(View):
-
-    def get(self, _request, filename):
-        path = os.path.join(os.path.dirname(__file__), 'static', filename)
-
-        if os.path.isfile(path):
-            with open(path, 'rb') as file:
-                return HttpResponse(file.read(), content_type='application/javascript')
-        else:
-            return HttpResponseNotFound()
